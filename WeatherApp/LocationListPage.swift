259107b2912fbcdd5c10e792f4b69066734a5c32
Udit Kapoor <uditkapoor060@gmail.com>
2026-01-23 10:12:17 -0800

    implemented the landing page

---
+++ WeatherApp/LocationListPage.swift
@@ -0,0 +1,18 @@
+//
+//  LocationListPage.swift
+//  WeatherApp
+//
+//  Created by rentamac on 1/23/26.
+//
+
+import SwiftUI
+
+struct LocationListPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationListPage()
+}
